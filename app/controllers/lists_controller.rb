49ad7b16364553164c2b49487b368d798f388a93
lhsharing <daley.luis@gmail.com>
2019-05-09 13:43:14 -0400

    + show method in lists index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,19 @@
 class ListsController < ApplicationController
   def index
+    @list = List.new
     @lists = List.all
 #    raise @lists.inspect
     # implicit rendering
     # render 'site/index'
     # render 'controller/action'
+    # render 'lists/index.html.erb'
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+#raise params.inspect      
   end
 end

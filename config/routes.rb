aa30205a3beb84e0a7b8137f2c9e7c3793dce802
lhsharing <daley.luis@gmail.com>
2019-05-11 19:55:01 -0400

    +items for RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists
+  get 'items/create'
+  resources :lists do #creates 7 restful routes.  to view enter 'rake routes' in terminal.
+  resources :items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'lists#index'
 end

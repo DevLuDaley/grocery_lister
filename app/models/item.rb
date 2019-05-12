b6c9d20fcb79ce7c9405a9c37a0d521647de514a
lhsharing <daley.luis@gmail.com>
2019-05-12 13:02:18 -0400

    description valdiation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
 #class Item  < ActiveRecord::Base
     belongs_to :list #adds methods to my model
+    validates :description, presence: true, length: { minimum: 2 }
+
 end
+ 

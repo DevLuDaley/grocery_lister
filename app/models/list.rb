9dd9163ebc1cd67e92c27b7088d2423d85229fa3
lhsharing <daley.luis@gmail.com>
2019-05-12 13:04:55 -0400

    aded character minimum

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
 #class List < ActiveRecord::Base
     has_many :items #adds methods to my model
     #validates :name, presence => true
-    validates :name, presence: true
+    validates :name, presence: true, length: { minimum: 2 }
 #validates :presence => :name
 end
+
+

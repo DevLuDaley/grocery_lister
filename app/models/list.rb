1bff7e815771e763ce1e070ce523ce71ad7db4cf
lhsharing <daley.luis@gmail.com>
2019-05-11 20:44:33 -0400

    +list :name, presence: true validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ApplicationRecord
 #class List < ActiveRecord::Base
     has_many :items #adds methods to my model
+    #validates :name, presence => true
+    validates :name, presence: true
+#validates :presence => :name
 end

34b3c1d0750ed917b13e282790d92f6ef596dfaf
lhsharing <daley.luis@gmail.com>
2019-05-13 10:45:20 -0400

    +STATUS hash to enable [:complete] instead of '1'

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,20 @@ class Item < ApplicationRecord
     validates :description, presence: true, length: { minimum: 2 }
     validates :description, uniqueness: true
 
-
+    STATUS = {
+        :incomplete => 0,
+        :complete => 1
+    }
+    #use hash above to set up key 10:00 v3
+    #clearly define meaning of #0 || #1 in context
     def complete?
-        self.status == 1
+        self.status == STATUS[:complete]    
+        #self.status == 1
     end
 
     def incomplete?
-        self.status == 0
+        self.status == STATUS[:incomplete]
+        #self.status == 0
     end
-
 
 end

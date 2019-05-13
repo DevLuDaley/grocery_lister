fc099e948a1dfb435a9989df9ed3f800e8229065
lhsharing <daley.luis@gmail.com>
2019-05-13 10:02:32 -0400

    description uniqueness validation in List creation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
 #class Item  < ActiveRecord::Base
     belongs_to :list #adds methods to my model
     validates :description, presence: true, length: { minimum: 2 }
+    validates :description, uniqueness: true
 
 end
  

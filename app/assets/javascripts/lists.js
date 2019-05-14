cbe37d5eeec4422528dd2f2169c5d20a36c9f7eb
lhsharing <daley.luis@gmail.com>
2019-05-14 11:34:08 -0400

    removed junk comments from code

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,12 +1,7 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the `rails generate channel` command.
-//
-//= require action_cable
-//= require_self
-//= require_tree ./channels
-
 $(function(){
   $("input.toggle").on("change", function () {
     $(this).parents("form").trigger("submit")
   })
+  
 });
+
